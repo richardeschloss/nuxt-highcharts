1a358364fb9f9cfc62a0d5203c1d14be5857bd65
Richard Schloss <richard.e.schloss@gmail.com>
2023-02-20 22:19:43 -0500

    Added basic example for composition-api

---
+++ components/charts/BasicComposition.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <label>Basic composition</label>
+    <highchart :options="chartOptions" exporting :modules="['exporting']"/>
+  </div>
+</template>
+
+<script setup>
+const chartOptions = reactive({
+  title: {
+    text: 'Some Text'
+  },
+  series: [{
+    data: [[0, 0], [1, 1]]
+  }]  
+});
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>s
